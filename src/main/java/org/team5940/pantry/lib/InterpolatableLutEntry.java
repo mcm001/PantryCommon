69f8e4b0d51e67a359aa821bc28f543c167c1130
Matthew Morley <matthew.morley.ca@gmail.com>
2019-05-12 16:43:31 -0700

    misc cleanup

--- src/main/java/org/team5940/pantry/lib/InterpolatableLutEntry.java
+++ src/main/java/org/team5940/pantry/lib/InterpolatableLutEntry.java
@@ -1,27 +1,27 @@
- package org.team5940.pantry.lib;
+package org.team5940.pantry.lib;
 
- import org.ghrobotics.lib.mathematics.MathExtensionsKt;
- import org.ghrobotics.lib.types.Interpolatable;
+import org.ghrobotics.lib.mathematics.MathExtensionsKt;
+import org.ghrobotics.lib.types.Interpolatable;
 
- public class InterpolatableLutEntry implements Interpolatable<Double> {
+public class InterpolatableLutEntry implements Interpolatable<Double> {
 
- 	Double value;
+	Double value;
 
- 	public Double getValue() {
- 		return value;
- 	}
+	public Double getValue() {
+		return value;
+	}
 
- 	public InterpolatableLutEntry(Double value) {
- 		this.value = value;
- 	}
+	public InterpolatableLutEntry(Double value) {
+		this.value = value;
+	}
 
- 	public InterpolatableLutEntry(double value) {
- 		this(Double.valueOf(value));
- 	}
+	public InterpolatableLutEntry(double value) {
+		this(Double.valueOf(value));
+	}
 
- 	@Override
- 	public Double interpolate(Double otherVal, double arg1) {
- 		return MathExtensionsKt.lerp(value, otherVal, arg1);
- 	}
+	@Override
+	public Double interpolate(Double otherVal, double arg1) {
+		return MathExtensionsKt.lerp(value, otherVal, arg1);
+	}
 
- }
+}
